8f28696ec63b7251c1fffbe1391acdd67225421c
Tomasz Wojtczak <tomek.wojtczak13@gmail.com>
2024-08-19 19:32:03 +0200

    version 1.1.0
    D

--- ipkgen.sh
+++ ipkgen.sh
@@ -7,17 +7,24 @@
 #                                                   #
 #####################################################
 
-# Variables
+## Variables (If you change filenames package might not work!)
+
+# File names for data. (files to install)
 DATA_SOURCE="./data"
 DATA_ARCHIVE="data.tar.gz"
 
+# File names for package info.
 CONTROL_SOURCE="./control"
 CONTROL_ARCHIVE="control.tar.gz"
 
+# Version and filename for debian-binary version file. (!!! Change version only if older system !!!)
 DEBIAN_BINARY=2.0
+DEBIAN_BINARY_NAME="debian-binary"
 
-VERSION=1.0
+# Package version
+VERSION=1.0.0
 
+# If no arguments then display help
 if [ "$1" = "" ]
 then
   echo "ipk creation script version $VERSION"
@@ -27,6 +34,7 @@ then
   echo "options:"
   echo "        -wipe                              deletes all sources"
 else
+  # If the first argument is option -wipe then clear all files for new package!
   if [ "$1" = "-wipe" ]
   then
     echo "[Warning]: Deleting sources, archives and ipk files!"
@@ -35,28 +43,36 @@ else
     rm -rf "${CONTROL_ARCHIVE}"
     rm -rf "${DATA_SOURCE}"
     rm -rf "${CONTROL_SOURCE}"
-    rm -rf "debian-binary"
+    rm -rf "${DEBIAN_BINARY_NAME}"
     mkdir "${DATA_SOURCE}"
     mkdir "${CONTROL_SOURCE}"
-    echo "#!/bin/sh" >> "$CONTROL_SOURCE/postinst"
-    echo "#!/bin/sh" >> "$CONTROL_SOURCE/preinst"
-    echo "#!/bin/sh" >> "$CONTROL_SOURCE/prerm"
+    echo "#!/bin/sh" > "${CONTROL_SOURCE}/postinst"
+    echo "#!/bin/sh" > "${CONTROL_SOURCE}/preinst"
+    echo "#!/bin/sh" > "${CONTROL_SOURCE}/prerm"
+    echo "Package: helloworldc" > "${CONTROL_SOURCE}/control"
+    echo "Version: 1.0.0" > "${CONTROL_SOURCE}/control"
+    echo "Architecture: x86" > "${CONTROL_SOURCE}/control"
+    echo "Maintainer: user@example.com" > "${CONTROL_SOURCE}/control"
+    echo "Description: This is example description." > "${CONTROL_SOURCE}/control"
+    echo "Priority: optional" > "${CONTROL_SOURCE}/control"
+    echo "Dep: systemctl systemd" > "${CONTROL_SOURCE}/control"
     echo "" >> "$CONTROL_SOURCE/control"
-    chmod +x "$CONTROL_SOURCE/postinst"
-    chmod +x "$CONTROL_SOURCE/preinst"
-    chmod +x "$CONTROL_SOURCE/prerm"
+    chmod +x "${CONTROL_SOURCE}/postinst"
+    chmod +x "${CONTROL_SOURCE}/preinst"
+    chmod +x "${CONTROL_SOURCE}/prerm"
   else
+    # else create ipk package:
     NAME="$1"
     VER="$2"
     ARCH="$3"
     IPK_NAME="${NAME}_${VER}_${ARCH}.ipk"
     echo "[Info]: Creating subarchives."
-    tar -czvf "$DATA_ARCHIVE" $DATA_SOURCE/*
-    tar -czvf "$CONTROL_ARCHIVE" ${CONTROL_SOURCE}/*
-    echo "[Info]: Creating debian-binary file."
-    touch debian-binary
-    echo "$DEBIAN_BINARY" >> "debian-binary"
+    tar -czvf "$DATA_ARCHIVE" "$DATA_SOURCE/*"
+    tar -czvf "$CONTROL_ARCHIVE" "${CONTROL_SOURCE}/*"
+    echo "[Info]: Creating ${DEBIAN_BINARY_NAME} file."
+    touch "${DEBIAN_BINARY_NAME}"
+    echo "$DEBIAN_BINARY" >> "${DEBIAN_BINARY_NAME}"
     echo "[Info]: Creating main ipk archive."
-    tar --numeric-owner --group=0 --owner=0 -cf ./"${IPK_NAME}" ./debian-binary "$DATA_ARCHIVE" "$CONTROL_ARCHIVE"
+    tar --numeric-owner --group=0 --owner=0 -cf ./"${IPK_NAME}" ./debian-binary "${DATA_ARCHIVE}" "${CONTROL_ARCHIVE}"
   fi
 fi
